8dbbfcfa20feeba03bf488793df5dc9c9809b490
Takeshi Horiuchi <keshihoriuchi@gmail.com>
2019-05-26 08:43:06 +0900

    Solve Leap

--- leap/src/lib.rs
+++ leap/src/lib.rs
@@ -1,3 +1,11 @@
 pub fn is_leap_year(year: u64) -> bool {
-    unimplemented!("true if {} is a leap year", year)
+  if year % 400 == 0 {
+    true
+  } else if year % 100 == 0 {
+    false
+  } else if year % 4 == 0 {
+    true
+  } else {
+    false
+  }
 }
