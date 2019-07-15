14772e697ae71e91c9747271f9722a3b3be96e19
Takeshi Horiuchi <keshihoriuchi@gmail.com>
2019-07-15 17:21:17 +0900

    Solve "grains"

--- grains/src/lib.rs
+++ grains/src/lib.rs
@@ -1,7 +1,10 @@
 pub fn square(s: u32) -> u64 {
-    unimplemented!("grains of rice on square {}", s);
+    if s < 1 || 64 < s {
+        panic!("Square must be between 1 and 64")
+    }
+    2u64.pow(s - 1)
 }
 
 pub fn total() -> u64 {
-    unimplemented!();
+    18_446_744_073_709_551_615
 }
