00763ba6e86967f54eb0aca4331717c941e82198
Takeshi Horiuchi <keshihoriuchi@gmail.com>
2019-07-27 21:56:12 +0900

    Solve pig-latin

--- pig-latin/src/lib.rs
+++ pig-latin/src/lib.rs
@@ -1,6 +1,44 @@
+#![warn(clippy::all)]
 pub fn translate(input: &str) -> String {
-    unimplemented!(
-        "Using the Pig Latin text transformation rules, convert the given input '{}'",
-        input
-    );
+    let vec: Vec<String> = input.split(' ').map(process_word).collect();
+    vec.join(" ")
+}
+
+fn process_word(input: &str) -> String {
+    let cvec: Vec<char> = input.chars().collect();
+    let mut i = 0;
+    while i < cvec.len() {
+        if i == 0 && cvec.len() >= 2 {
+            if let ('x', 'r') | ('y', 't') = (cvec[0], cvec[1]) {
+                break;
+            }
+        }
+        if let 'a' | 'i' | 'e' | 'o' = cvec[i] {
+            break;
+        }
+        if cvec[i] == 'u' && (i == 0 || cvec[i - 1] != 'q') {
+            break;
+        }
+        i += 1;
+    }
+    let mut iy = 0;
+    while iy < i {
+        if cvec[iy] == 'y' {
+            break;
+        }
+        iy += 1;
+    }
+
+    let mut s = String::new();
+    for c in &cvec[i..] {
+        s.push(*c)
+    }
+    for c in &cvec[iy..i] {
+        s.push(*c)
+    }
+    for c in &cvec[..iy] {
+        s.push(*c)
+    }
+    s.push_str("ay");
+    s
 }
