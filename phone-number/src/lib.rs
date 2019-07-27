f7e0a18fb0f1154399f534a560b7c05bd8e28b9c
Takeshi Horiuchi <keshihoriuchi@gmail.com>
2019-07-27 18:50:18 +0900

    Solve phone-number

--- phone-number/src/lib.rs
+++ phone-number/src/lib.rs
@@ -1,6 +1,20 @@
 pub fn number(user_number: &str) -> Option<String> {
-    unimplemented!(
-        "Given the number entered by user '{}', convert it into SMS-friendly format. If the entered number is not a valid NANP number, return None.",
-        user_number
-    );
+    let (c, len) = user_number
+        .chars()
+        .fold((vec![], 0), |(mut acc, len), x| match x {
+            '1' if len == 0 => (acc, len),
+            '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {
+                acc.push(x);
+                (acc, len + 1)
+            }
+            _ => (acc, len),
+        });
+    if c[0] == '0' || c[0] == '1' || c[3] == '0' || c[3] == '1' {
+        None
+    } else if len == 10 {
+        let s: String = c.iter().collect();
+        Some(s)
+    } else {
+        None
+    }
 }
--- phone-number/tests/phone-number.rs
+++ phone-number/tests/phone-number.rs
@@ -10,55 +10,46 @@ fn test_cleans_the_number() {
 }
 
 #[test]
-#[ignore]
 fn test_cleans_numbers_with_dots() {
     process_clean_case("223.456.7890", Some("2234567890"));
 }
 
 #[test]
-#[ignore]
 fn test_cleans_numbers_with_multiple_spaces() {
     process_clean_case("223 456   7890   ", Some("2234567890"));
 }
 
 #[test]
-#[ignore]
 fn test_invalid_when_9_digits() {
     process_clean_case("123456789", None);
 }
 
 #[test]
-#[ignore]
 fn test_invalid_when_11_digits_does_not_start_with_a_1() {
     process_clean_case("22234567890", None);
 }
 
 #[test]
-#[ignore]
 fn test_valid_when_11_digits_and_starting_with_1() {
     process_clean_case("12234567890", Some("2234567890"));
 }
 
 #[test]
-#[ignore]
 fn test_valid_when_11_digits_and_starting_with_1_even_with_punctuation() {
     process_clean_case("+1 (223) 456-7890", Some("2234567890"));
 }
 
 #[test]
-#[ignore]
 fn test_invalid_when_more_than_11_digits() {
     process_clean_case("321234567890", None);
 }
 
 #[test]
-#[ignore]
 fn test_invalid_with_letters() {
     process_clean_case("123-abc-7890", None);
 }
 
 #[test]
-#[ignore]
 fn test_invalid_with_punctuations() {
     process_clean_case("123-@:!-7890", None);
 }
