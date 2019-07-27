f3862183410a5080b218df599caf552dd86bf849
Takeshi Horiuchi <keshihoriuchi@gmail.com>
2019-07-27 22:32:38 +0900

    Refactor

--- phone-number/src/lib.rs
+++ phone-number/src/lib.rs
@@ -1,16 +1,16 @@
 #![warn(clippy::all)]
 pub fn number(user_number: &str) -> Option<String> {
-    let (c, len) = user_number
+    let mut c: Vec<char> = user_number
         .chars()
-        .fold((vec![], 0), |(mut acc, len), x| match x {
-            '1' if len == 0 => (acc, len),
-            '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {
-                acc.push(x);
-                (acc, len + 1)
-            }
-            _ => (acc, len),
-        });
-    if c[0] == '0' || c[0] == '1' || c[3] == '0' || c[3] == '1' || len != 10 {
+        .filter(|x| match x {
+            '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => true,
+            _ => false,
+        })
+        .collect();
+    if c[0] == '1' && c.len() == 11 {
+        c.remove(0);
+    }
+    if c[0] == '0' || c[0] == '1' || c[3] == '0' || c[3] == '1' || c.len() != 10 {
         None
     } else {
         Some(c.iter().collect())
