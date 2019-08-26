3ec9104bf120f8551183af29b67b30fdc2c939f0
Takeshi Horiuchi <keshihoriuchi@gmail.com>
2019-08-27 07:35:42 +0900

    temp

--- wasm-audio/www/webpack.config.js
+++ wasm-audio/www/webpack.config.js
@@ -2,10 +2,13 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require('path');
 
 module.exports = {
-  entry: "./bootstrap.js",
+  entry: {
+    bootstrap: "./bootstrap.js",
+    "wasm-worklet": "./wasm-worklet.js"
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bootstrap.js",
+    filename: "[name].js",
   },
   mode: "development",
   plugins: [
