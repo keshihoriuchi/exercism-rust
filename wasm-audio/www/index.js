6918ff611c973c417112bbe2901756a30fbaf3e0
Takeshi Horiuchi <keshihoriuchi@gmail.com>
2019-08-27 03:00:25 +0900

    add web audio

--- wasm-audio/www/index.js
+++ wasm-audio/www/index.js
@@ -1,3 +1,14 @@
 import * as wasm from "wasm-audio";
 
+const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+navigator.getUserMedia(
+  { audio: true },
+  stream => {
+    const src = audioCtx.createMediaStreamSource(stream);
+    src.connect(audioCtx.destination);
+  },
+  err => console.error(err)
+);
+
 wasm.greet();
