e4b7b1c6806ce49d743644fcc5508f180c5a4a48
Takeshi Horiuchi <keshihoriuchi@gmail.com>
2019-09-15 04:46:01 +0900

    Fix for nested loop

--- brainfxck-cli/src/main.rs
+++ brainfxck-cli/src/main.rs
@@ -3,17 +3,39 @@
 
 extern crate test;
 
+use std::collections::HashMap;
+use std::fs;
 use std::io::Read;
 use std::io::Write;
-use std::fs;
 
 pub fn brainfxck<T: Read, U: Write>(s: &str, input: &mut T, output: &mut U) {
     let mut buf: [u8; 30000] = [0; 30000];
-    let mut ptr = 0;
-    let mut bracket_stack: Vec<usize> = vec![];
+    let mut ptr = 1000;
     let mut pc = 0;
-    let src: Vec<char> = s.chars().collect();
+    let src: Vec<char> = s
+        .chars()
+        .filter(|c| match c {
+            '>' | '<' | '+' | '-' | '.' | ',' | '[' | ']' => true,
+            _ => false,
+        })
+        .collect();
     let len = src.len();
+    let mut bracket_stack: Vec<usize> = vec![];
+    let mut jump_table = HashMap::new();
+    while pc < len {
+        match src[pc] {
+            '[' => bracket_stack.push(pc),
+            ']' => {
+                let left_pc = bracket_stack.pop().unwrap();
+                let right_pc = pc;
+                jump_table.insert(left_pc, right_pc);
+                jump_table.insert(right_pc, left_pc);
+            }
+            _ => (),
+        }
+        pc += 1;
+    }
+    pc = 0;
     while pc < len {
         match src[pc] {
             '>' => ptr += 1,
@@ -30,18 +52,14 @@ pub fn brainfxck<T: Read, U: Write>(s: &str, input: &mut T, output: &mut U) {
             }
             '[' => {
                 if buf[ptr] == 0 {
-                    while src[pc] != ']' {
-                        pc += 1;
-                    }
-                } else {
-                    bracket_stack.push(pc);
+                    pc = *jump_table.get(&pc).unwrap();
                 }
             }
             ']' => {
-                pc = bracket_stack.pop().unwrap();
+                pc = *jump_table.get(&pc).unwrap();
                 continue;
             }
-            _ => {}
+            _ => panic!(),
         }
         pc += 1;
     }
@@ -66,6 +84,25 @@ mod tests {
         assert_eq!("Hello World!\n", std::str::from_utf8(&v).unwrap());
     }
 
+    #[test]
+    fn mul_to_8() {
+        let mut v = vec![];
+        super::brainfxck(
+            r#"
+                ++++>++><<
+                [-
+                  >[->>+<<]
+                  >>[-<+<+>>]
+                  <<<
+                ]>>
+                ++++++++++++++++++++++++++++++++++++++++++++++++.
+            "#,
+            &mut std::io::stdin(),
+            &mut v,
+        );
+        assert_eq!("8", std::str::from_utf8(&v).unwrap());
+    }
+
     #[bench]
     fn bench_brainfxck(b: &mut Bencher) {
         b.iter(|| {
@@ -75,14 +112,13 @@ mod tests {
                 >+++++++++[<++++++++>-]<.>+++++++[<++++>-]<+.+++++++..+++.[-]>++++++++[<++
                 ++>-]<.>+++++++++++[<+++++>-]<.>++++++++[<+++>-]<.+++.------.--------.[-]>
                 ++++++++[<++++>-]<+.[-]++++++++++.
-            "#,
+                "#,
                 &mut std::io::stdin(),
                 &mut v,
             );
         });
     }
 }
-
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
