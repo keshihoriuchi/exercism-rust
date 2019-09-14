74a9230380a2c7684d12b87540b5b24e566fab94
Takeshi Horiuchi <keshihoriuchi@gmail.com>
2019-09-15 05:22:41 +0900

    Introduce intermediate commands

--- brainfxck-cli/src/main.rs
+++ brainfxck-cli/src/main.rs
@@ -3,63 +3,120 @@
 
 extern crate test;
 
-use std::collections::HashMap;
 use std::fs;
 use std::io::Read;
 use std::io::Write;
 
-pub fn brainfxck<T: Read, U: Write>(s: &str, input: &mut T, output: &mut U) {
+enum Command {
+    RShift(usize),
+    LShift(usize),
+    Inc(u8),
+    Dec(u8),
+    PutC,
+    GetC,
+    WhileS(usize),
+    WhileE(usize),
+}
+
+pub fn brainfxck<R: Read, W: Write>(s: &str, input: &mut R, output: &mut W) {
     let mut buf: [u8; 30000] = [0; 30000];
     let mut ptr = 1000;
     let mut pc = 0;
-    let src: Vec<char> = s
-        .chars()
-        .filter(|c| match c {
-            '>' | '<' | '+' | '-' | '.' | ',' | '[' | ']' => true,
-            _ => false,
-        })
-        .collect();
-    let len = src.len();
+    let src: Vec<char> = s.chars().collect();
+    let mut len = src.len();
     let mut bracket_stack: Vec<usize> = vec![];
-    let mut jump_table = HashMap::new();
+    let mut commands: Vec<Command> = vec![];
     while pc < len {
         match src[pc] {
-            '[' => bracket_stack.push(pc),
+            '>' => {
+                let mut v = 1;
+                loop {
+                    pc += 1;
+                    if src[pc] != '>' {
+                        commands.push(Command::RShift(v));
+                        break;
+                    }
+                    v += 1;
+                }
+                continue;
+            }
+            '<' => {
+                let mut v = 1;
+                loop {
+                    pc += 1;
+                    if src[pc] != '<' {
+                        commands.push(Command::LShift(v));
+                        break;
+                    }
+                    v += 1;
+                }
+                continue;
+            }
+            '+' => {
+                let mut v = 1;
+                loop {
+                    pc += 1;
+                    if src[pc] != '+' {
+                        commands.push(Command::Inc(v));
+                        break;
+                    }
+                    v += 1;
+                }
+                continue;
+            }
+            '-' => {
+                let mut v = 1;
+                loop {
+                    pc += 1;
+                    if src[pc] != '-' {
+                        commands.push(Command::Dec(v));
+                        break;
+                    }
+                    v += 1;
+                }
+                continue;
+            }
+            '.' => commands.push(Command::PutC),
+            ',' => commands.push(Command::GetC),
+            '[' => {
+                bracket_stack.push(commands.len());
+                commands.push(Command::WhileS(0));
+            }
             ']' => {
                 let left_pc = bracket_stack.pop().unwrap();
-                let right_pc = pc;
-                jump_table.insert(left_pc, right_pc);
-                jump_table.insert(right_pc, left_pc);
+                commands[left_pc] = Command::WhileS(commands.len());
+                commands.push(Command::WhileE(left_pc));
             }
             _ => (),
         }
         pc += 1;
     }
+
+    len = commands.len();
     pc = 0;
     while pc < len {
-        match src[pc] {
-            '>' => ptr += 1,
-            '<' => ptr -= 1,
-            '+' => buf[ptr] += 1,
-            '-' => buf[ptr] -= 1,
-            '.' => {
+        match commands[pc] {
+            Command::RShift(n) => ptr += n,
+            Command::LShift(n) => ptr -= n,
+            Command::Inc(n) => buf[ptr] += n,
+            Command::Dec(n) => buf[ptr] -= n,
+            Command::PutC => {
                 output.write_all(&[buf[ptr]]).unwrap();
             }
-            ',' => {
+            Command::GetC => {
                 let mut b = [0; 1];
                 input.read_exact(&mut b).unwrap();
                 buf[ptr] = b[0];
             }
-            '[' => {
+            Command::WhileS(n) => {
                 if buf[ptr] == 0 {
-                    pc = *jump_table.get(&pc).unwrap();
+                    pc = n;
                 }
             }
-            ']' => {
-                pc = *jump_table.get(&pc).unwrap();
+            Command::WhileE(n) => {
+                pc = n;
                 continue;
             }
-            _ => panic!(),
         }
         pc += 1;
     }
